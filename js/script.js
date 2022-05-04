ee727baffd87f3fe79cd912635479a65c881144b
David <DavidHenryDev@gmail.com>
2022-05-04 19:57:19 -0400

    fixed form sizing and started on JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-document.querySelector(".fa-user").addEventListener('click', scrollTo)
-document.querySelector(".fa-user").addEventListener('click', scrollTo)
-document.querySelector(".fa-user").addEventListener('click', scrollTo)
+// document.querySelector(".about").addEventListener('click', scrollTo)
+// document.querySelector(".work").addEventListener('click', scrollTo)
+// document.querySelector(".contact").addEventListener('click', scrollTo)
 
+document.querySelectorAll(".listen").forEach((item) => {
+    item.addEventListener('click', scrollTo )
+})
 
 function scrollTo() {
-    document.querySelector('.aboutMe').scrollIntoView({ behavior: "smooth"})
+    if (document.querySelector('.work') ){
+    document.querySelector('#myProjects').scrollIntoView({ behavior: "smooth"})
+    } 
 }
