cfb461e73acc01f361536a5bc09c7edd18d925b1
David <davidhenrydev@gmail.com>
2022-09-18 19:51:10 -0400

    added mobile navigation, various improvements

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
-const sliders = document.querySelectorAll(".slide-in")
-const faders = document.querySelectorAll(".fade-in")
+const sliders = document.querySelectorAll('.slide-in')
+const faders = document.querySelectorAll('.fade-in')
+const navToggle = document.querySelector('.nav-toggle')
+const navMenu = document.querySelector('.nav-menu')
 
-
-  const appearOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px -150px 0px"
-  };
-  
-  const appearOnScroll = new IntersectionObserver(function(
-    entries,
-    appearOnScroll
-  ) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        entry.target.classList.add("appear");
-        appearOnScroll.unobserve(entry.target);
-      }
-    });
-  },
-  appearOptions);
-  
-
-sliders.forEach(slider => {
-    appearOnScroll.observe(slider)
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active')
+  navMenu.classList.toggle('active')
 })
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader)
+document.querySelectorAll('.link').forEach((n) =>
+  n.addEventListener('click', () => {
+    navToggle.classList.remove('active')
+    navMenu.classList.remove('active')
+  })
+)
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: '0px 0px -150px 0px',
+}
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return
+    } else {
+      entry.target.classList.add('appear')
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+},
+appearOptions)
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider)
+})
+
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader)
 })
